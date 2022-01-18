adfee8b508e21ad07ec8a143ce6fe39ad9bd696d
x19g011 <x19g011@mitsuhashi.ac.jp>
2022-01-18 13:29:16 +0900

    x19g011 ccc

--- app/src/main/java/com/example/thebookuser/SearchBactivity.java
+++ app/src/main/java/com/example/thebookuser/SearchBactivity.java
@@ -20,11 +20,38 @@ public class SearchBactivity  extends AppCompatActivity implements AdapterView.O
             R.drawable.hibana
     };
 
+    private static final String[] author = {
+            "又吉直樹"
+    };
+    private static final String[] isbn = {
+            "4167907828"
+    };
+
+    private static final String[] lebel = {
+            "文春文庫",
+
+    };
+    private static final String[] publisher = {
+            "文藝春秋"
+    };
+    private static final String[] money = {
+            "660",
+
+    };
+    private static final String[] genre = {
+            "文学",
+
+    };
+
+    private static final String[] hangata = {
+            "文庫判",
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.search);
         // ListViewのインスタンスを生成
         ListView listView = findViewById(R.id.list_view);
 
@@ -45,10 +72,24 @@ public class SearchBactivity  extends AppCompatActivity implements AdapterView.O
         Intent intent = new Intent(this.getApplicationContext(), SearchActivity.class);
         // clickされたpositionのtextとphotoのID
         String selectedText = scenes[position];
+        String selectedText3 = "著者名:"+author[position];
+        String selectedText2 = "ISBN:"+isbn[position];
+        String selectedText4 = "レーベル:"+lebel[position];
+        String selectedText5 = "出版社:"+publisher[position];
+        String selectedText6 = "値段:"+money[position]+"円(税込)";
+        String selectedText7 = "ジャンル:"+ genre[position];
+        String selectedText8 = "判型:"+hangata[position];
         int selectedPhoto = photos[position];
         // インテントにセット
         intent.putExtra("Text", selectedText);
         intent.putExtra("Photo", selectedPhoto);
+        intent.putExtra("Text2", selectedText2);
+        intent.putExtra("Text3", selectedText3);
+        intent.putExtra("Text4", selectedText4);
+        intent.putExtra("Text5", selectedText5);
+        intent.putExtra("Text6", selectedText6);
+        intent.putExtra("Text7", selectedText7);
+        intent.putExtra("Text8", selectedText8);
         // Activity をスイッチする
         startActivity(intent);
     }
